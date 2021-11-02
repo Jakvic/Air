2e9afe5110326bc9c5e64ea980d09d83f5c9d0e8
Jakvic <nsj520@outlook.com>
2021-11-02 17:11:26 +0800

    Add a custom control -> AirButton

---
+++ AirControl/AirButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AirControl
+{
+    public enum AirButtonType
+    {
+        Main,
+        Default
+    }
+    
+    public class AirButton : Button
+    {
+        static AirButton()
+        {
+            // see https://stackoverflow.com/questions/1228875/what-is-so-special-about-generic-xaml
+            // so I need to create a generic.xaml in themes
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AirButton),
+                new FrameworkPropertyMetadata(typeof(AirButton)));
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            "CornerRadius", typeof(CornerRadius), typeof(AirButton), new PropertyMetadata(default(CornerRadius)));
+        
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius) GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty TypeProperty = DependencyProperty.Register(
+            "Type", typeof(AirButtonType), typeof(AirButton), new PropertyMetadata(default(AirButtonType)));
+
+        public AirButtonType Type
+        {
+            get { return (AirButtonType) GetValue(TypeProperty); }
+            set { SetValue(TypeProperty, value); }
+        }
+        
+    }
+}
---
+++ AirControl/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+// this can remove the prefix of controls in this proj
+// for example: old time -> `controls:AirButton`, now: AirButton
+// very nice :)
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation","AirControl")]
+
