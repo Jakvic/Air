762a9dae6e00f76f17c75fa317cd7102d1176c36
Jakvic <nsj520@outlook.com>
2021-11-20 12:57:10 +0800

    +

--- AirControl/AirTextBox.cs
+++ AirControl/AirTextBox.cs
@@ -8,10 +8,19 @@ namespace AirControl
         public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
             "CornerRadius", typeof(CornerRadius), typeof(AirTextBox), new PropertyMetadata(default(CornerRadius)));
 
+        public static readonly DependencyProperty HelpTextProperty = DependencyProperty.Register(
+            "HelpText", typeof(string), typeof(AirTextBox), new PropertyMetadata(default(string)));
+
         static AirTextBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(AirTextBox),
                 new FrameworkPropertyMetadata(typeof(AirTextBox)));
+        }
+
+        public string HelpText
+        {
+            get => (string) GetValue(HelpTextProperty);
+            set => SetValue(HelpTextProperty, value);
         }
 
         public CornerRadius CornerRadius
