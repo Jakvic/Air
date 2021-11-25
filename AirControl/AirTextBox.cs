939ea8d2de6610062f29b0995906b35e82183465
Jakvic <nsj520@outlook.com>
2021-11-25 21:07:53 +0800

    Fade in/out animation  of helptext in AirTextBox

--- AirControl/AirTextBox.cs
+++ AirControl/AirTextBox.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Animation;
 
 namespace AirControl
@@ -18,55 +20,6 @@ namespace AirControl
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(AirTextBox),
                 new FrameworkPropertyMetadata(typeof(AirTextBox)));
-        }
-
-        public TextBlock templateChild { get; set; }
-
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-            templateChild = (GetTemplateChild("HelpTextBox") as TextBlock)!;
-            templateChild.IsVisibleChanged += TemplateChildOnIsVisibleChanged;
-        }
-
-        private void TemplateChildOnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            var frameworkElement = sender as FrameworkElement;
-            if (frameworkElement is null)
-            {
-                return;
-            }
-
-            return;
-            var doubleAnimation = new DoubleAnimation
-            {
-                Duration = new Duration(TimeSpan.FromMilliseconds(2000))
-            };
-            if (frameworkElement.Visibility is Visibility.Hidden)
-            {
-                if (frameworkElement.Opacity == 0)
-                {
-                    return;
-                }
-
-                doubleAnimation.From = frameworkElement.Opacity;
-                doubleAnimation.To = 0.0;
-            }
-            else
-            {
-                if (frameworkElement.Opacity >= 0)
-                {
-                    return;
-                }
-
-                doubleAnimation.From = 0.0;
-                doubleAnimation.To = frameworkElement.Opacity;
-            }
-
-            var storyboard = new Storyboard();
-            storyboard.Children.Add(doubleAnimation);
-            frameworkElement.BeginAnimation(OpacityProperty, doubleAnimation);
-            storyboard.Children.Clear();
         }
 
         public string HelpText
