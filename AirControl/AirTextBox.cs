af712b22b18fdaf2900ea517dc5d5581f7c40735
Jakvic <nsj520@outlook.com>
2021-11-25 15:22:40 +0800

    Animation for textbox (test code)

--- AirControl/AirTextBox.cs
+++ AirControl/AirTextBox.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace AirControl
 {
@@ -15,6 +18,55 @@ namespace AirControl
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(AirTextBox),
                 new FrameworkPropertyMetadata(typeof(AirTextBox)));
+        }
+
+        public TextBlock templateChild { get; set; }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            templateChild = (GetTemplateChild("HelpTextBox") as TextBlock)!;
+            templateChild.IsVisibleChanged += TemplateChildOnIsVisibleChanged;
+        }
+
+        private void TemplateChildOnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var frameworkElement = sender as FrameworkElement;
+            if (frameworkElement is null)
+            {
+                return;
+            }
+
+            return;
+            var doubleAnimation = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromMilliseconds(2000))
+            };
+            if (frameworkElement.Visibility is Visibility.Hidden)
+            {
+                if (frameworkElement.Opacity == 0)
+                {
+                    return;
+                }
+
+                doubleAnimation.From = frameworkElement.Opacity;
+                doubleAnimation.To = 0.0;
+            }
+            else
+            {
+                if (frameworkElement.Opacity >= 0)
+                {
+                    return;
+                }
+
+                doubleAnimation.From = 0.0;
+                doubleAnimation.To = frameworkElement.Opacity;
+            }
+
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(doubleAnimation);
+            frameworkElement.BeginAnimation(OpacityProperty, doubleAnimation);
+            storyboard.Children.Clear();
         }
 
         public string HelpText
---
+++ AirControl/VisibilityAnimation.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Animation;
+
+namespace AirControl
+{
+    public class VisibilityAnimation
+    {
+        private const int AnimationDuration = 300;
+
+        private static readonly Dictionary<FrameworkElement, bool> hookedElements =
+            new();
+
+        static VisibilityAnimation()
+        {
+            UIElement.VisibilityProperty.AddOwner(typeof(FrameworkElement), new FrameworkPropertyMetadata(
+                Visibility.Visible,
+                null, CoerceVisibility));
+        }
+
+        private static void HookVisibilityChanges(FrameworkElement frameworkElement)
+        {
+            hookedElements.Add(frameworkElement, false);
+        }
+
+        private static void UnHookVisibilityChanges(FrameworkElement frameworkElement)
+        {
+            if (hookedElements.ContainsKey(frameworkElement))
+            {
+                hookedElements.Remove(frameworkElement);
+            }
+        }
+
+        private static object CoerceVisibility(DependencyObject dependencyObject, object baseValue)
+        {
+            var frameworkElement = dependencyObject as FrameworkElement;
+            if (frameworkElement is null)
+            {
+                return baseValue;
+            }
+
+            var visibility = (Visibility) baseValue;
+            if (visibility == frameworkElement.Visibility)
+            {
+                return baseValue;
+            }
+
+            if (!IsHookedElement(frameworkElement))
+            {
+                return baseValue;
+            }
+
+            if (UpdateAnimationStartedFlag(frameworkElement))
+            {
+                return baseValue;
+            }
+
+            var doubleAnimation = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromMilliseconds(AnimationDuration))
+            };
+
+            doubleAnimation.Completed += (s, e) =>
+            {
+                if (visibility is Visibility.Visible)
+                {
+                    UpdateAnimationStartedFlag(frameworkElement);
+                }
+                else
+                {
+                    if (BindingOperations.IsDataBound(frameworkElement, UIElement.VisibilityProperty))
+                    {
+                        var binding = BindingOperations.GetBinding(frameworkElement, UIElement.VisibilityProperty);
+                        BindingOperations.SetBinding(frameworkElement, UIElement.VisibilityProperty, binding!);
+                    }
+                    else
+                    {
+                        frameworkElement.Visibility = visibility;
+                    }
+                }
+            };
+
+            if (visibility is Visibility.Collapsed or Visibility.Hidden)
+            {
+                doubleAnimation.From = 1.0;
+                doubleAnimation.To = 0.0;
+            }
+            else
+            {
+                doubleAnimation.From = 0.0;
+                doubleAnimation.To = 1.0;
+            }
+
+            frameworkElement.BeginAnimation(UIElement.OpacityProperty, doubleAnimation);
+            return Visibility.Visible;
+        }
+
+        private static bool IsHookedElement(FrameworkElement frameworkElement)
+        {
+            return hookedElements.ContainsKey(frameworkElement);
+        }
+
+        private static bool UpdateAnimationStartedFlag(FrameworkElement frameworkElement)
+        {
+            var started = hookedElements[frameworkElement];
+            hookedElements[frameworkElement] = !started;
+            return started;
+        }
+    }
+}
