97323fbfd31dd8888c35030aa4937111e99a5ae8
Jakvic <nsj520@outlook.com>
2021-12-06 15:34:19 +0800

    CornerRadius Progressbar

--- AirControl/AirProgressBar.cs
+++ AirControl/AirProgressBar.cs
@@ -1,13 +1,29 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace AirControl
 {
     public class AirProgressBar : ProgressBar
     {
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(AirProgressBar),
+                new PropertyMetadata(default(CornerRadius)));
+
         static AirProgressBar()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(AirProgressBar), 
-                new System.Windows.PropertyMetadata(typeof(AirProgressBar)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AirProgressBar),
+                new FrameworkPropertyMetadata(typeof(AirProgressBar)));
         }
+
+        /// <summary>
+        ///     THe `Indicator` & `Animation` Rectangle's RadiusX binding CornerRadius.TopLeft
+        ///     RadiusY binding CornerRadius.TopRight
+        /// </summary>
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius) GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
     }
 }
---
+++ AirControl/Convertors/WidthHeight2RectConvertor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace AirControl.Convertors
+{
+    public class WidthHeight2RectConvertor : MarkupExtension, IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] == DependencyProperty.UnsetValue)
+            {
+                return null;
+            }
+            return new Rect(0, 0, (double) values[0], (double) values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
