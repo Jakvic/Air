0836df5a1e1526267d7f3578a39dac74ddb0f212
Junkie High <nsj520@outlook.com>
2021-12-28 00:23:03 +0800

    Learn about TabControl&TabItem

--- Air/MainWindow.xaml.cs
+++ Air/MainWindow.xaml.cs
@@ -18,24 +18,23 @@ namespace Air
         private Thickness rect_raw_margin;
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            rect_raw_margin = rect.Margin;
-            rect_raw_margin.Left -= rect.Width;
+
         }
 
         private void StartAnimation()
         {
-            Storyboard sb = new();
-            var thicknessAnimation = new ThicknessAnimation
-            {
-                Duration = new Duration(TimeSpan.FromMilliseconds(2000)),
-                From = rect_raw_margin,
-                To = new Thickness(border.Width, rect.Margin.Top,
-                    -rect.Width, rect.Margin.Bottom),
-                RepeatBehavior = RepeatBehavior.Forever,
-            };
-            Storyboard.SetTargetProperty(thicknessAnimation, new PropertyPath("Margin"));
-            sb.Children.Add(thicknessAnimation);
-            sb.Begin(rect);
+            /*Storyboard sb = new();
+                        var thicknessAnimation = new ThicknessAnimation
+                        {
+                            Duration = new Duration(TimeSpan.FromMilliseconds(2000)),
+                            From = rect_raw_margin,
+                            To = new Thickness(border.Width, rect.Margin.Top,
+                                -rect.Width, rect.Margin.Bottom),
+                            RepeatBehavior = RepeatBehavior.Forever,
+                        };
+                        Storyboard.SetTargetProperty(thicknessAnimation, new PropertyPath("Margin"));
+                        sb.Children.Add(thicknessAnimation);
+                        sb.Begin(rect);*/
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
