c8c4bc8d1e91c83a0b9292db44107007011399fa
Jakvic <nsj520@outlook.com>
2022-01-05 19:04:11 +0800

    +

--- AirControl/AirScrollBar.cs
+++ AirControl/AirScrollBar.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls.Primitives;
 
 namespace AirControl
 {
     public class AirScrollBar: ScrollBar
     {
+        static AirScrollBar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AirScrollBar),new PropertyMetadata(typeof(AirScrollBar)));
+        }
     }
 }
