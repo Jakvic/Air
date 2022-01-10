7cd7531c10e65e7c6dc2ea8d6ea3aaaa929c2945
Jakvic <nsj520@outlook.com>
2022-01-10 14:28:27 +0800

    Add AirScrollViewer

---
+++ AirControl/AirScrollViewer.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AirControl
+{
+    public class AirScrollViewer : ScrollViewer
+    {
+        static AirScrollViewer()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AirScrollViewer),
+                new PropertyMetadata(typeof(AirScrollViewer)));
+        }
+    }
+}
