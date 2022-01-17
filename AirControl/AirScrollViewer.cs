b43f7559c7af5b82099c3a9d9f566bc23d8082f6
Jakvic <nsj520@outlook.com>
2022-01-17 21:48:27 +0800

    Bugs in AirScrollViewer & AirScrollBar

--- AirControl/AirScrollViewer.cs
+++ AirControl/AirScrollViewer.cs
@@ -5,10 +5,19 @@ namespace AirControl
 {
     public class AirScrollViewer : ScrollViewer
     {
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            "CornerRadius", typeof(CornerRadius), typeof(AirScrollViewer), new PropertyMetadata(default(CornerRadius)));
+
         static AirScrollViewer()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(AirScrollViewer),
                 new FrameworkPropertyMetadata(typeof(AirScrollViewer)));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius) GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
         }
     }
 }
