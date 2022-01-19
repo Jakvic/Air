1f4d2edcfdc0aea38efdf7cf44f4f3d7780624d4
Jakvic <nsj520@outlook.com>
2022-01-20 00:30:21 +0800

    animation fixing

--- AirControl/AirScrollViewer.cs
+++ AirControl/AirScrollViewer.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace AirControl
 {
@@ -23,14 +27,78 @@ namespace AirControl
 
         public CornerRadius ScrollBarCornerRadius
         {
-            get => (CornerRadius) GetValue(ScrollBarCornerRadiusProperty);
+            get => (CornerRadius)GetValue(ScrollBarCornerRadiusProperty);
             set => SetValue(ScrollBarCornerRadiusProperty, value);
         }
 
         public double ScrollBarWidth
         {
-            get => (double) GetValue(ScrollBarWidthProperty);
+            get => (double)GetValue(ScrollBarWidthProperty);
             set => SetValue(ScrollBarWidthProperty, value);
         }
+
+        Border animationBorder;
+        Border hitTestBorder;
+        Storyboard startStoryboard;
+        Storyboard endStoryboard;
+        ScrollBar vsb;
+        ScrollBar hsb;
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            vsb = GetTemplateChild("PART_VerticalScrollBar") as ScrollBar;
+            hsb = GetTemplateChild("PART_HorizontalScrollBar") as ScrollBar;
+
+            vsb.ApplyTemplate();
+            animationBorder = vsb.Template.FindName("BorderRoot", vsb) as Border;
+            vsb.ApplyTemplate();
+            hitTestBorder = vsb.Template.FindName("HitTestBorder", vsb) as Border;
+
+            hitTestBorder.MouseEnter += ScrollBar_MouseEnter;
+            hitTestBorder.MouseLeave += ScrollBar_MouseLeave;
+        }
+
+        private void ScrollBar_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            endStoryboard?.Stop();
+            startStoryboard = new Storyboard();
+            var animation = new DoubleAnimation(ScrollBarWidth, 10, new Duration(TimeSpan.FromMilliseconds(100)));
+            Storyboard.SetTarget(animation, animationBorder);
+            var scrollBar = vsb ?? hsb;
+            if (scrollBar.Orientation is Orientation.Vertical)
+            {
+                Storyboard.SetTargetProperty(animation, new PropertyPath("Width"));
+                startStoryboard.Children.Add(animation);
+            }
+            else if (scrollBar.Orientation is Orientation.Horizontal)
+            {
+                Storyboard.SetTargetProperty(animation, new PropertyPath("Height"));
+                startStoryboard.Children.Add(animation);
+            }
+            startStoryboard.Begin();
+
+        }
+        private void ScrollBar_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var scrollBar = vsb ?? hsb;
+            startStoryboard?.Stop();
+            endStoryboard?.Stop();
+            endStoryboard = new Storyboard();
+            var animation = new DoubleAnimation(10, ScrollBarWidth, new Duration(TimeSpan.FromMilliseconds(100)));
+            Storyboard.SetTarget(animation, animationBorder);
+            if (scrollBar.Orientation is Orientation.Vertical)
+            {
+                Storyboard.SetTargetProperty(animation, new PropertyPath("Width"));
+                endStoryboard.Children.Add(animation);
+            }
+            else if (scrollBar.Orientation is Orientation.Horizontal)
+            {
+                Storyboard.SetTargetProperty(animation, new PropertyPath("Height"));
+                endStoryboard.Children.Add(animation);
+            }
+            endStoryboard.Begin();
+        }
+
     }
 }
