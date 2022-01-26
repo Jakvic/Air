9d53bfa305fe9bf50c8dd2764f1c04f223bb6511
Jakvic <nsj520@outlook.com>
2022-01-26 14:13:58 +0800

    Add AirTabControl & AirTabItem

---
+++ AirControl/AirTabControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AirControl
+{
+    public class AirTabControl : TabControl
+    {
+        static AirTabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AirTabControl),
+                new FrameworkPropertyMetadata(typeof(AirTabControl)));
+        }
+    }
+}
---
+++ AirControl/AirTabItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AirControl
+{
+    public class AirTabItem : TabItem
+    {
+        static AirTabItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AirTabItem), new FrameworkPropertyMetadata(typeof(AirTabItem)));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(AirTabItem), new PropertyMetadata(default));
+
+    }
+}
