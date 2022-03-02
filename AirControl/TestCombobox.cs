c140de7573100548e5fb015d746d82e88574adba
Jakvic <nsj520@outlook.com>
2022-03-02 22:08:21 +0800

    +

--- AirControl/TestCombobox.cs
+++ AirControl/TestCombobox.cs
@@ -3,11 +3,21 @@ using System.Windows.Controls;
 
 namespace AirControl
 {
-    public class TestCombobox : ComboBox 
+    public class TestCombobox : ComboBox
     {
-        static  TestCombobox() 
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            "CornerRadius", typeof(CornerRadius), typeof(TestCombobox), new PropertyMetadata(default(CornerRadius)));
+
+        static TestCombobox()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(TestCombobox),new FrameworkPropertyMetadata(typeof(TestCombobox)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TestCombobox),
+                new FrameworkPropertyMetadata(typeof(TestCombobox)));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius) GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
         }
     }
 }
