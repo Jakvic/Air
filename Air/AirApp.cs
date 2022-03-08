1d38ea07193d457687ca9db78e98d450b5355e21
Jakvic <nsj520@outlook.com>
2022-03-08 23:12:59 +0800

    _

---
+++ Air/AirApp.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows;
+
+namespace Air
+{
+    public class AirApp : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        }
+    }
+}
--- Air/App.xaml.cs
+++ Air/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
 
 namespace Air
 {
@@ -7,6 +10,15 @@ namespace Air
     /// </summary>
     public partial class App : Application
     {
-        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var singleOrDefault = assemblies.SingleOrDefault(x=>x.GetName().Name == "Air");
+            var enumerable = singleOrDefault.GetTypes().FirstOrDefault(x=>x.IsAbstract && x.Name == "ViewModel");
+            foreach (var type in singleOrDefault.GetTypes().Where(x=>x.IsSubclassOf(enumerable)))
+            {
+            }
+        }
     }
 }
