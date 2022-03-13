5cc72798b974f2dfd2f73a2d35849fc8732a946b
Jakvic <nsj520@outlook.com>
2022-03-13 23:58:43 +0800

    --

--- AirControl/AirTreeView.cs
+++ AirControl/AirTreeView.cs
@@ -1,9 +1,14 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace AirControl
 {
     public class AirTreeView : TreeView
     {
-        
+        static AirTreeView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AirTreeView),
+                new FrameworkPropertyMetadata(typeof(AirTreeView)));
+        }
     }
 }
