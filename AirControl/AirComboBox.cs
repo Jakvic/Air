2d8551f111df73934529cf12f880bab52835ae63
Jakvic <nsj520@outlook.com>
2022-04-26 23:07:17 +0800

    still combobox

--- AirControl/AirComboBox.cs
+++ AirControl/AirComboBox.cs
@@ -18,6 +18,7 @@ namespace AirControl
         {
             get => (CornerRadius) GetValue(CornerRadiusProperty);
             set => SetValue(CornerRadiusProperty, value);
+            
         }
     }
 }
