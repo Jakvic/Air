be8591f2b8567335f4a1b92196c23250005a7b69
Jakvic <nsj520@outlook.com>
2023-07-05 17:45:45 +0800

    Add Caption

--- AirControl/AirCaption.cs
+++ AirControl/AirCaption.cs
@@ -1,5 +1,21 @@
-﻿namespace AirControl;
+﻿using System.Windows;
+using System.Windows.Controls;
 
-public class AirCaption
+namespace AirControl;
+
+public class AirCaption : ContentControl
 {
+    static AirCaption()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(AirCaption), new FrameworkPropertyMetadata(typeof(AirCaption)));
+    }
+
+    public static readonly DependencyProperty SystemButtonWidthProperty = DependencyProperty.Register(
+        nameof(SystemButtonWidth), typeof(double), typeof(AirCaption), new PropertyMetadata(default(double)));
+
+    public double SystemButtonWidth
+    {
+        get => (double)GetValue(SystemButtonWidthProperty);
+        set => SetValue(SystemButtonWidthProperty, value);
+    }
 }
