14dab2875744a39bce158d5b6c881a12b119023a
Jakvic <nsj520@outlook.com>
2023-07-11 17:26:01 +0800

    Fix AirDataGrid

--- Air.WPFDemo/Table_Model.cs
+++ Air.WPFDemo/Table_Model.cs
@@ -11,6 +11,7 @@ public class Table_Model : ViewModel<Table>
         {
             new User(1, "Jack", new DateTime(1991, 1, 1)),
             new User(3, "Desmond", new DateTime(1954, 3, 23)),
-            new User(5, "Malen", new DateTime(2000, 11, 12))
+            new User(5, "Malen", new DateTime(2000, 11, 12)),
+            new User(7, "Ivan", new DateTime(1984, 7, 21))
         };
 }
