377b57de80d15b220d34e1cd37bd1087c38f469f
Jakvic <nsj520@outlook.com>
2023-09-06 10:47:20 +0800

    adjust main view layout

--- Air.WPFDemo/App.xaml.cs
+++ Air.WPFDemo/App.xaml.cs
@@ -11,12 +11,12 @@ public partial class App
         base.OnStartup(e);
         var mainModel = new Main_Model().CreateWindow();
         mainModel.WindowStyle = WindowStyle.SingleBorderWindow;
-        WindowChrome.SetWindowChrome(mainModel, new WindowChrome
-        {
-            CaptionHeight = 0,
-            UseAeroCaptionButtons = true,
-            GlassFrameThickness = new Thickness(10)
-        });
+        //WindowChrome.SetWindowChrome(mainModel, new WindowChrome
+        //{
+        //    CaptionHeight = 0,
+        //    UseAeroCaptionButtons = true,
+        //    GlassFrameThickness = new Thickness(10)
+        //});
 
         //如果这里不指定Application.MainWindow，WPF默认会用NavigationWindow作为父窗体
         Service.Register(new AppService(mainModel));
